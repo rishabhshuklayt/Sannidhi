b193f39d452fd3a8aa2a37cd5e5354d1b8700508
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-06-22 15:38:51 +0530

    Test Email Adapter for console

---
+++ core/adapters/EmailAdapters/console_Email_adapter.py
@@ -0,0 +1,7 @@
+from core.ports.email_port import EmailPort
+# from django.core.mail import send_mail
+
+class ConsoleEmailAdapter(EmailPort):
+    def send_email(self, subject: str, message: str, from_email: str, recipient_list: list):
+        print(f"Sending email with subject: {subject}")
+        # send_mail(subject, message, from_email, recipient_list)
