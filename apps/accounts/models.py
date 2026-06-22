edddae1673e3342dbad594e26f3b1135131574b3
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-06-22 15:31:32 +0530

    added db sqlite

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractUser, BaseUserManager, PermissionsMixin)
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
