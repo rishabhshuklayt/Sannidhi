1a3c488039b8f035125852a6b316132cafc66566
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-06-28 00:03:15 +0530

    write logic for Simple_JWT_Adapter

--- core/adapters/JwtAdapters/simple_jwt_adapter.py
+++ core/adapters/JwtAdapters/simple_jwt_adapter.py
@@ -1,6 +1,11 @@
 from core.ports.jwt_port.jwt_port import JWTPort
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class Simple_JWT_Adapter(JWTPort):
-   def generate_token(self, user_id: int) -> str:
-      pass
-   
+   def generate_token(self, user_id):
+      refresh_token = RefreshToken.for_user(user_id)
+      access_token = refresh_token.access_token
+      return {
+         "refresh_token": str(refresh_token),
+         "access_token": str(access_token)
+      }
