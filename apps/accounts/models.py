778c807610978d148e01a23d3998620b5edbbb64
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-06-28 00:03:51 +0530

    removed designation for now

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import (AbstractUser, BaseUserManager, Permissio
 # Create your models here.
 
 class User(AbstractUser):
-    
-    designation = models.CharField(max_length=100, blank=True, null=True)
+    pass
+    # designation = models.CharField(max_length=100, blank=True, null=True)
     
