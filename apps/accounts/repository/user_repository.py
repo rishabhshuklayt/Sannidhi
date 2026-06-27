1dc63de2b7e020298c710c793834085803dcac40
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-06-27 22:48:26 +0530

    added multiple function for user operations in repository

--- apps/accounts/repository/user_repository.py
+++ apps/accounts/repository/user_repository.py
@@ -13,3 +13,30 @@ class UserRepository:
             password=password,
             username=username
         )
+    
+    def delete_user(self, email: str):
+        user = self.get_by_email(email=email)
+        if user:
+            user.delete()
+            return True
+        return False
+    
+    def update_user(self, email: str, **kwargs):
+        user = self.get_by_email(email=email)
+        if user:
+            for key, value in kwargs.items():
+                setattr(user, key, value)
+            user.save()
+            return user
+        return None
+    
+    def get_all_users(self):
+        return User.objects.all()
+    
+    def deactivate_user(self, email: str):
+        user = self.get_by_email(email=email)
+        if user:
+            user.is_active = False
+            user.save()
+            return user
+        return None
