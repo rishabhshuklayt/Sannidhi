78112702229a59b342c8fc9d61544d99942e1407
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-07-04 00:28:20 +0530

    add tenants repository

---
+++ apps/MultiTenants/repository/tenants_repository.py
@@ -0,0 +1,22 @@
+
+from apps.MultiTenants.models import Tenants
+
+
+class TenantsRepository:
+    def __init__(self):
+        pass
+
+    def create_tenant(self, name, tenant_type, plan , email, domain, metadata, private_metadata, is_active, is_exempted):
+        
+      tenant = Tenants.objects.create(
+        name=name,
+        tenant_type=tenant_type,
+        plan=plan,
+        email=email,
+        domain=domain,
+        metadata=metadata,
+        private_metadata=private_metadata,
+        is_active=is_active,
+        is_exempted=is_exempted
+      )
+      return tenant
