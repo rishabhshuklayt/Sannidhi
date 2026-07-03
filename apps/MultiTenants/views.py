a5f4964f4ea2e0250bed8e0c0bdbad7d498c235a
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-07-03 23:44:58 +0530

    updated format of view of geofence

--- apps/MultiTenants/views.py
+++ apps/MultiTenants/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.views import APIView, Response
+
+from apps.MultiTenants.serializers.createTenantSerializer import CreateTenantSerializer
 
 # Create your views here.
 
-class BaseTenantView(APIView):
-    def dispatch(self, request, *args, **kwargs):
-        # here i will write the Validations and permissions checks
-        return super().dispatch(request, *args, **kwargs)
-    pass
 
-class CreateTenant(BaseTenantView):
-    # here i will write buisnedd logic 
-    pass
+class CreateTenant(APIView):
+    """
+    API view to create a new tenant.
+    """
+    def post(self, request, *args, **kwargs):
+        # Logic to create a new tenant goes here
+        serializer = CreateTenantSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        email = serializer.validated_data.get("email")
+        
+        # Perform tenant creation logic here
+        return Response({"message": "Tenant created successfully."}, status=201)
