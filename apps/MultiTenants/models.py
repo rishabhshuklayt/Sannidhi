dc54efb9dfe540b6dedc3ae0d2c365e4aaaec003
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-07-04 19:56:17 +0530

    fixed data type and length of the domain

--- apps/MultiTenants/models.py
+++ apps/MultiTenants/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 class Tenants(models.Model):
     name = models.CharField(max_length=100)
-    domain = models.CharField(max_length=100, unique=True)
+    domain = models.EmailField(max_length=100, unique=True)
     client_uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     metadata = models.JSONField(blank=True, null=True)
     plan = models.CharField(max_length=100, null=True)
     tenant_type = models.CharField(max_length=100,null=True)
