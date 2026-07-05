f29f159f4aac2bfb83ca35926343df53b7225538
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-07-05 10:34:46 +0530

    added urls

--- apps/MultiTenants/urls.py
+++ apps/MultiTenants/urls.py
@@ -4,4 +4,6 @@ from apps.MultiTenants.views import CreateTenant
 
 urlpatterns = [
     path('create-tenant/', CreateTenant.as_view(), name='create_tenant'),
+    path('get-tenant/<int:tenant_id>/', CreateTenant.as_view(), name='get_tenant'),
+   
 ]
