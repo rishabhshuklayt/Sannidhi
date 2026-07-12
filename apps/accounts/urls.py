09683abd89407ff93bd541082b67cb604c1d6aa7
Rishabh Shukla <rishabhshuklayt@gmail.com>
2026-07-13 00:02:36 +0530

    added multiple new routes for future use

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
-from .views import RegisterView , LoginView
+from .views import RegisterView , LoginView, ProfileUpdateView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path("login/", LoginView.as_view(), name="login")
-    path("logout/", LoginView.as_view(), name="logout")
+    path("login/", LoginView.as_view(), name="login"),
+    # path("logout/", LoginView.as_view(), name="logout")
     # path("reset-password/", LoginView.as_view(), name="reset-password")
     # path("change-password/", LoginView.as_view(), name="change-password")
     # path("verify-email/", LoginView.as_view(), name="verify-email")
@@ -11,7 +11,7 @@ urlpatterns = [
     # path("refresh-token/", LoginView.as_view(), name="refresh-token")
     # path("revoke-token/", LoginView.as_view(), name="revoke-token")
     # path("get-user-profile/", LoginView.as_view(), name="get-user-profile")
-    # path("update-user-profile/", ProfileUpdateView.as_view(), name="update-user-profile")
+    path("update-user-profile/", ProfileUpdateView.as_view(), name="update-user-profile")
     # path("delete-user-account/", LoginView.as_view(), name="delete-user-account")
     # path("list-users/", LoginView.as_view(), name="list-users")
     # path("get-user-by-id/<int:user_id>/", LoginView.as_view(), name="get-user-by-id")
